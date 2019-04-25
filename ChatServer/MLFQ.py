4c8ed00069787ad7b7010be1782bcd8f8d953a1a
hakidehari <hakidehari@outlook.com>
2019-04-25 17:55:55 -0500

    MLFQ class commit

---
+++ ChatServer/MLFQ.py
@@ -0,0 +1,17 @@
+
+
+class MLFQ:
+
+    def __init__(self):
+        self.mlfq = {}
+
+    def incrementMsgCount(self):
+        pass
+
+
+    def lowerPriority(self, user):
+        pass
+
+    def isLegalMessage(self, user):
+        pass
+
