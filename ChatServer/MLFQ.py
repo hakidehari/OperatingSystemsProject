faa1b30141a1f1aec759c951c8f7f1b1606dc55c
hakidehari <hakidehari@outlook.com>
2019-04-27 14:39:37 -0500

    changes

--- ChatServer/MLFQ.py
+++ ChatServer/MLFQ.py
@@ -6,17 +6,45 @@ class MLFQ:
         self.msges = {}
         self.priority = {}
 
-    def incrementMsgCount(self, user):
-        if user not in self.msges:
-            self.msges[user] = 1
-        else:
-            self.msges[user] += 1
 
 
     def lowerPriority(self, user):
-        pass
+        if self.msges[user] == 5:
+            self.priority[user] = 2
+            return True
+        if self.msges[user] == 8:
+            self.priority[user] = 3
+            return True
+        if self.msges[user] == 9:
+            self.priority[user] = 4
+            return True
+
 
     def isLegalMessage(self, user):
-        pass
+        if user not in self.priority:
+            self.priority[user] = 1
+            return True
+        else:
+            if self.priority[user] < 4:
+                return True
+        return False
+
+
+    def incrementMsgCount(self, user):
+        dict = {}
+        if self.isLegalMessage(user):
+            if user not in self.msges:
+                self.msges[user] = 1
+            else:
+                self.msges[user] += 1
+            if self.lowerPriority(user):
+                dict['True'] = True
+                dict['loweredPriority'] = 1
+                return dict
+            dict['True'] = True
+            return dict
+        else:
+            dict['False'] = False
+            return dict
 
 
--- ChatServer/Main.py
+++ ChatServer/Main.py
@@ -18,8 +18,23 @@ def messageReceived(methods=['GET', 'POST']):
 def handle_my_custom_event(json, methods=['GET', 'POST']):
     print('received my event: ' + str(json))
     if 'user_name' in json:
-        print(json['user_name'])
-    socketio.emit('my response', json, callback=messageReceived)
+        user = json['user_name']
+        w0t = mlfq.incrementMsgCount(user)
+        if 'True' in w0t:
+            socketio.emit('my response', json, callback=messageReceived)
+        if 'False' in w0t:
+            json['message'] = "You have reached your message limit.  This chat will now self destruct in 5 seconds."
+            socketio.emit('my response', json, callback=messageReceived)
+        if 'loweredPriority' in w0t:
+            if mlfq.priority[user] == 4:
+                json['message'] = "Your priority level is 4. These messages will now self destruct in 5 seconds."
+                socketio.emit('my response', json, callback=messageReceived)
+            else:
+                json['message'] = "You have been lowered to priority level " + str(mlfq.priority[user]) + ".  Be careful or your messages will self destruct!"
+                socketio.emit('my response', json, callback=messageReceived)
+
+
+
 
 if __name__ == '__main__':
     socketio.run(app, debug=True)
